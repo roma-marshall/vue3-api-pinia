8e5c0447cf31afee494b701d1baaebe182cf358d
roma_marshall <roma_marshall@outlook.com>
2024-06-18 13:44:40 +0200

    added tech stack

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Technology Stack used: Vue3, REST API and Pinia.
 ## Recommended Installation
 
 ```
-git clone https://github.com/roma-marshall/json-api-pinia.git
+git clone https://github.com/roma-marshall/vue3-api-pinia
 cd json-api-pinia
 npm run dev
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <div class="container grid md:grid-cols-3 gap-10 mx-5 md:mx-auto my-20">
-    <div v-for="item in json">
-      <div class="flex flex-col justify-center space-y-2">
-        <div class="text-center cursor-pointer w-fit h-fit space-y-2 mx-auto">
-          <img
-              class="rounded-xl shadow-lg saturate-50 hover:saturate-100 hover:scale-105 transition-all duration-500"
-              :src="item.thumbnail"
-              :alt="item.slug"
-          >
-          <span class="text-gray-300 text-xs">{{ item.slug }}</span>
-        </div>
+  <div class="container my-20 mx-5 md:mx-auto">
+    <div class="flex flex-col my-20 text-center space-y-2">
+      <span class="text-2xl">
+        The open source project example using the technology stack: Vue3, REST API + Pinia store
+      </span>
+      <a
+          target="_blank"
+          href="https://github.com/roma-marshall/vue3-api-pinia"
+          class="text-base text-blue-700 hover:text-blue-500">
+        https://github.com/roma-marshall/vue3-api-pinia
+      </a>
+    </div>
+    <div class="grid md:grid-cols-3 gap-10">
+      <div v-for="item in json">
+        <div class="flex flex-col justify-center space-y-2">
+          <div class="text-center cursor-pointer w-fit h-fit space-y-2 mx-auto">
+            <img
+                class="rounded-xl shadow-lg saturate-50 hover:saturate-100 hover:scale-105 transition-all duration-500"
+                :src="item.thumbnail"
+                :alt="item.slug"
+            >
+            <span class="text-gray-300 text-xs">{{ item.slug }}</span>
+          </div>
 
-        <div class="text-gray-700 text-sm text-center">
-          {{ item.title }}
-        </div>
+          <div class="text-gray-700 text-sm text-center">
+            {{ item.title }}
+          </div>
 
-        <div class="text-gray-300 text-xs text-center">
-          {{ item.publishedAt }}
+          <div class="text-gray-300 text-xs text-center">
+            {{ item.publishedAt }}
+          </div>
         </div>
       </div>
     </div>
